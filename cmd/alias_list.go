f42e1ec97b3a9c7cb4b5f212b2345b4622493747
Pelagie-AINTANGAR <pelagie.aintangar@laplateforme.io>
2026-09-17 14:31:24 +0200

    feat(alias): add alias command shells

---
+++ cmd/alias_list.go
@@ -0,0 +1,13 @@
+package cmd
+
+import "github.com/spf13/cobra"
+
+var aliasListCmd = &cobra.Command{
+	Use:   "list",
+	Short: "List connection profiles",
+	Long:  "List connection profiles and their endpoints.",
+	Args:  cobra.NoArgs,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		return nil
+	},
+}
