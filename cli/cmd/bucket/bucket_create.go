6ed954f6b3211ae0edf99e37524b9a86be3c0d54
Arthur Descourvieres <arthur.descourvieres@laplateforme.io>
2026-09-18 11:35:07 +0200

    refactor(cli): range les commandes bucket dans cli/cmd/bucket/
    Les commandes bucket rejoignent le rangement en sous-dossier deja adopte pour object et alias. Seul le chemin change, le comportement est identique.

---
+++ cli/cmd/bucket/bucket_create.go
@@ -0,0 +1,25 @@
+package bucket
+
+import (
+	"fmt"
+
+	"github.com/ArthurDescourvieres/plateforme-mycli/cli/internal/s3"
+	"github.com/spf13/cobra"
+)
+
+var CreateBucket = &cobra.Command{
+	Use:   "create",
+	Short: "Create a bucket",
+	Long:  "Create a new bucket in the S3 storage. This command requires the name of the bucket to be specified using the --bucket flag.",
+	Run: func(cmd *cobra.Command, args []string) {
+		s3.CreateBucket(bucketName)
+		fmt.Fprintf(cmd.OutOrStdout(), "Bucket created: %s\n", bucketName)
+	},
+}
+
+var bucketName string
+
+func init() {
+	CreateBucket.Flags().StringVarP(&bucketName, "bucket", "b", "", "Name of the bucket")
+	_ = CreateBucket.MarkFlagRequired("bucket")
+}
